b77423edbfc95889f772e5e80e187c7c5ede117b
Ander-RL <anderrodriguez205@gmail.com>
2024-03-14 15:27:04 +0100

    Product controller pagination.

--- src/main/java/com/boxing/shop/react/controller/ProductController.java
+++ src/main/java/com/boxing/shop/react/controller/ProductController.java
@@ -3,10 +3,14 @@ package com.boxing.shop.react.controller;
 import com.boxing.shop.react.dto.GetProductDto;
 import com.boxing.shop.react.service.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -26,6 +30,27 @@ public class ProductController {
     public List<GetProductDto> getProducts(){
 
         return productService.getProducts();
+    }
+
+    /**
+     * Return paginated list of products
+     * @return List<GetProductDto>
+     */
+    @GetMapping(path = "/pages",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Page<GetProductDto> getProductsByPage(@RequestParam(name = "page",defaultValue = "0") int page,
+                                                 @RequestParam(name = "size", defaultValue = "3") int size){
+
+        Pageable pageRequest = PageRequest.of(page, size);
+        List<GetProductDto> allProducts = productService.getProducts();
+
+        // starting index
+        int start = (int) pageRequest.getOffset();
+        // ending index
+        int end = Math.min((start + pageRequest.getPageSize()), allProducts.size());
+        // sublist
+        List<GetProductDto> pageContent = allProducts.subList(start, end);
+
+        return new PageImpl<>(pageContent, pageRequest, allProducts.size());
     }
 
     /**
